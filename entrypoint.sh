70c4b963102a1838cf045a7680cc3ca6c059746a
ZhongCheng <zhongcheng@ZhongChengs-MacBook-Pro.local>
2015-08-11 00:17:20 +0800

    param the php configuration

---
+++ entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sed -i s/{{MYSQL_HOST}}/$MYSQL_HOST/ /var/www/html/phpMyAdmin/config.inc.php
+sed -i s/{{MYSQL_USER}}/$MYSQL_USER/ /var/www/html/phpMyAdmin/config.inc.php
+sed -i s/{{MYSQL_PASSWORD}}/$MYSQL_PASSWORD/ /var/www/html/phpMyAdmin/config.inc.php
+sed -i s/{{MYSQL_ALLOW_NO_PASSWORD}}/$MYSQL_ALLOW_NO_PASSWORD/ /var/www/html/phpMyAdmin/config.inc.php
